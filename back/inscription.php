a97a9870c96efc0363c17458f466583262928a57
Pays Audran <paysaudran@gmail.com>
2025-04-09 15:59:04 +0200

    first commit

---
+++ back/inscription.php
@@ -0,0 +1,40 @@
+<?php
+
+
+$host = 'localhost';        // Adresse du serveur MySQL
+$dbname = 'ecommerce'; // Nom de ta base de données
+$user = 'root';      // Ton identifiant MySQL
+$password = ''; // Ton mot de passe MySQL
+
+try {
+    // Connexion à la base de données avec PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+
+    echo "✅ Connexion réussie à la base de données.";
+} catch (PDOException $e) {
+    // En cas d’erreur, afficher le message
+    echo "❌ Erreur de connexion : " . $e->getMessage();
+}
+// Récupération des données du formulaire
+$nom = $_POST['username'] ?? '';
+$prenom= $_POST['name'] ?? '';
+$email = $_POST['email'] ?? '';
+$mdp= $_POST['password'] ?? '';
+
+    try {
+        // Requête d'insertion sécurisée
+        $sql = "INSERT INTO user (nom, prenom, email, mdp ) VALUES (:nom, :prenom, :email, :mdp)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            ':nom' => $nom,
+            ':prenom' => $prenom,
+            ':email' => $email,
+            ':mdp' => $mdp,
+        ]);
+    }
+//         echo "✅ Données enregistrées avec succès !";
+    catch (PDOException $e) {
+        echo "❌ Erreur lors de l'enregistrement : " . $e->getMessage();
+    }
+
+?>
