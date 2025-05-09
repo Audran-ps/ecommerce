c992a68bd4232b6a2d9b3e766faa1522e0ad90ce
Pays Audran <paysaudran@gmail.com>
2025-05-09 09:30:10 +0200

    css navbary

--- vue/navbar.php
+++ vue/navbar.php
@@ -1,27 +1,18 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-3">
-  <div class="container">
-    <a class="navbar-brand fw-bold text-primary" href="index.php">
-      <img src="logo.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top me-2">
-      MonSite
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<link rel="stylesheet" href="navbar.css"> <!-- Remplace ce chemin par le bon -->
 
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-      <ul class="navbar-nav gap-3">
-        <li class="nav-item">
-          <a class="nav-link text-dark fw-semibold hover-link" href="index.php">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-dark fw-semibold hover-link" href="login.php">Connexion</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link btn btn-outline-primary px-4 rounded-pill" href="inscription.php">S'inscrire</a>
-        </li>
-      </ul>
+<header>
+    <div class="logo">
+        <a href="category.php">N<span>anos</span></a>
     </div>
-  </div>
-</nav>
+    <ul class="menu">
+        <li><a href="category.php">Accueil</a></li>
+        <li><a href="inscription.php">Inscription</a></li>
+        <li><a href="conection.php">Connection</a></li>
+        <li><a href="shoppingcart.php">Pannier</a></li>
+    </ul>
+    <a href="reservation.php" class="btn-reservation">Réserver</a>
+
+    <!-- Menu responsive burger -->
+    <div class="responsive-menu" onclick="toggleMenu(this)"></div>
+</header>
 
